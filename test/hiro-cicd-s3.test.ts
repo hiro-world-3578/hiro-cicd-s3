75f8a0b071d54f241cec93000d44ca4951ca75f2
hiro-world-3578 <snowman3877@gmail.com>
2026-08-13 11:02:22 +0000

    Initial commit

---
+++ test/hiro-cicd-s3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HiroCicdS3 from '../lib/hiro-cicd-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hiro-cicd-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HiroCicdS3.HiroCicdS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
